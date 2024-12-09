b52eea624b71c127f2423f34def689a3759c0022
pta <pta.new26@gmail.com>
2024-12-09 21:05:59 +0300

    add black

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
 def sum_2(a: int, b: int) -> int:
   # тест функция
+  import random
   return a + b
--- test/test_sum_2.py
+++ test/test_sum_2.py
@@ -2,4 +2,7 @@ from src.main import sum_2
 
 def test_sum_2():
   result = sum_2(2,3)
+
+
+
   assert 5 == result
